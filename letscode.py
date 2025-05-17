9287cb7f9ac7f4b71b1ab15339fea5ad1987b966
Joshua Dimeji <90442838+JoshuaDimeji@users.noreply.github.com>
2025-05-17 03:00:59 +0100

    Add files via upload

---
+++ letscode.py
@@ -0,0 +1,20 @@
+# Let me use this comment to note some things that can help me now and in the future
+# print() function is for output in python
+# To print a string, use it like this, print("This is a string")
+# To print an assigned data, use print(age), where age is a variable name.
+# Variables are like containers use to store data
+# So you may not necessarily put the semicolon mark at the end of a statement in Python. I'll make it a practice to add to mine tho
+
+
+# Now, let's work
+
+print("This may look like a joke but I kid you not")
+age = 25; #this is an assignment
+
+name = "Ajoke"; #this is stored as string
+print(age);
+
+print(name) 
+print("will be")
+print(age)
+print("in few days to come")
---
+++ newfile.py
@@ -0,0 +1,6 @@
+# This particular one is to make comments
+# I'll still continue it like this
+
+
+
+print ("first output init");
